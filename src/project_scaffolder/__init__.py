e2c36f7bfa8c3f969c6505a71e939df513b8b01c
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 06:02:42 +0300

    feat: expose public Python API

---
+++ src/project_scaffolder/__init__.py
@@ -0,0 +1,6 @@
+"""Project Scaffolder public API."""
+from .core import ScaffoldResult, plan, scaffold, validate_name
+
+__version__ = "1.0.0"
+__author__ = "Radwan Abdulhadi Ahmed / @rad03i2"
+__all__ = ["ScaffoldResult", "plan", "scaffold", "validate_name"]
